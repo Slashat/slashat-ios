bd50e28a959a72465d7bf116907859adede589ff
Johan Larsson <johan@johanl.se>
2013-01-12 00:03:16 +0100

    Added temporary app icon

--- Slashat/AppDelegate.m
+++ Slashat/AppDelegate.m
@@ -14,6 +14,16 @@
 {
     _audioHandler = [[SlashatAudioHandler alloc] init];
     
+    [[UINavigationBar appearance] setTintColor:[UIColor colorWithRed:200/255.0f green:200/255.0f blue:200/255.0f alpha:1]];
+    
+    [[UINavigationBar appearance] setTitleTextAttributes: @{
+                                UITextAttributeTextColor: [UIColor colorWithRed:0.0/255.0 green:0.0/255.0 blue:0.0/255.0 alpha:1.0],
+                          UITextAttributeTextShadowColor: [UIColor colorWithRed:255.0 green:255.0 blue:255.0 alpha:0.8],
+                         UITextAttributeTextShadowOffset: [NSValue valueWithUIOffset:UIOffsetMake(0.0f, 1.0f)],
+                                     UITextAttributeFont: [UIFont fontWithName:@"Helvetica-Light" size:20.0f]
+     }];
+    
+    
     NSError *sessionError = nil;
     [[AVAudioSession sharedInstance] setDelegate:self];
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:&sessionError];
