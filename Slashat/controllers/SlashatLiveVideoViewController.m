0f060a4231b081281305cf923b2d3a9d4151ff0e
Johan Larsson <johan@johanl.se>
2013-02-03 15:04:04 +0100

    Hämtar nu senaste aktiva live-stream från Bambuser

--- Slashat/controllers/SlashatLiveVideoViewController.m
+++ Slashat/controllers/SlashatLiveVideoViewController.m
@@ -9,6 +9,7 @@
 #import "SlashatLiveVideoViewController.h"
 #import "APIKey.h"
 #import "MediaPlayer/MPMoviePlayerController.h"
+#import "AFJSONRequestOperation.h"
 
 @interface SlashatLiveVideoViewController ()
 
@@ -32,13 +33,59 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self fetchLiveStreams];
 }
 
 - (void)viewWillLayoutSubviews
 {
     [super viewWillLayoutSubviews];
     
-    _moviePlayer = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString:@"http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"]];
+    NSLog(@"Frame: %f, %f", self.view.frame.size.width, self.view.frame.size.height);
+    NSLog(@"Bounds: %f, %f", self.view.bounds.size.width, self.view.bounds.size.height);
+}
+
+- (void)fetchLiveStreams
+{
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://api.bambuser.com/broadcast.json?username=slashat&type=live&limit=1&api_key=%@", BAMBUSER_TRANSCODE_API_KEY]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        
+        NSArray *broadcasts = [JSON valueForKeyPath:@"result"];
+        
+        if (broadcasts.count > 0) {
+            NSString *broadcastId = [(id)[broadcasts objectAtIndex:0] valueForKeyPath:@"vid"];
+            NSLog(@"Live stream active: %@", broadcastId);
+            [self initializeLiveStream:broadcastId];
+        } else {
+            NSLog(@"No live streams active");
+        }
+
+    } failure:nil];
+    
+    [operation start];
+}
+
+- (void)initializeLiveStream:(NSString *)broadcastId
+{
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@.json?api_key=%@&preset=hls", BAMBUSER_TRANSCODE_URL, broadcastId, BAMBUSER_TRANSCODE_API_KEY]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        
+        NSLog(@"Bambuser url: %@", [[JSON valueForKeyPath:@"result"] valueForKeyPath:@"url"]);
+        [self playStream:[NSURL URLWithString:[[JSON valueForKeyPath:@"result"] valueForKeyPath:@"url"]]];
+    } failure:nil];
+    
+    [operation start];
+}
+
+- (void)playStream:(NSURL *)streamUrl
+{
+    _moviePlayer = [[MPMoviePlayerController alloc] initWithContentURL:streamUrl];
     [_moviePlayer setMovieSourceType:MPMovieSourceTypeStreaming];
     [_moviePlayer prepareToPlay];
     [_moviePlayer setControlStyle:MPMovieControlStyleEmbedded];
@@ -47,9 +94,6 @@
     [_moviePlayer.view setFrame:self.view.bounds];
     [self.view addSubview: _moviePlayer.view];
     [_moviePlayer play];
-    
-    NSLog(@"Frame: %f, %f", self.view.frame.size.width, self.view.frame.size.height);
-    NSLog(@"Bounds: %f, %f", self.view.bounds.size.width, self.view.bounds.size.height);
 }
 
 - (void)didReceiveMemoryWarning
