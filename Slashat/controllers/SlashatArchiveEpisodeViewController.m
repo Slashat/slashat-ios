16e98e36b612f274f994563139612c1d16a82276
Johan Larsson <johan@johanl.se>
2013-06-30 21:58:12 +0200

    Ny layout på episod-detailvy

--- Slashat/controllers/SlashatArchiveEpisodeViewController.m
+++ Slashat/controllers/SlashatArchiveEpisodeViewController.m
@@ -13,7 +13,11 @@
 
 @interface SlashatArchiveEpisodeViewController ()
 
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+
 @property (nonatomic, weak) IBOutlet UIButton *playButton;
+@property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+@property (nonatomic, weak) IBOutlet UILabel *dateLabel;
 @property (nonatomic, weak) IBOutlet UITextView *descriptionTextView;
 
 @end
@@ -32,12 +36,34 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
     
     self.navigationItem.title = self.episode.title;
-    NSLog(@"rssItem.mediaUrl: %@", self.episode.mediaUrl);
     
-    self.descriptionTextView.text = self.episode.showNotes;
+    self.descriptionTextView.contentInset = UIEdgeInsetsMake(-8,-8,-8,-8);
+    NSString *descriptionString = [NSString stringWithFormat:@"%@\n%@", self.episode.itemDescription, self.episode.showNotes];
+    self.descriptionTextView.text = descriptionString;
+    
+    self.titleLabel.text = [self.episode.title stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd"];
+    self.dateLabel.text = [formatter stringFromDate:self.episode.pubDate];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    CGRect frame = self.descriptionTextView.frame;
+    frame.size.height = self.descriptionTextView.contentSize.height;
+    self.descriptionTextView.frame = frame;
+    
+    CGFloat contentHeight = self.descriptionTextView.contentSize.height + self.descriptionTextView.frame.origin.y;
+    
+    CGSize contentSize = CGSizeMake(0, contentHeight);
+    
+    [self.scrollView setContentSize:contentSize];    
 }
 
 - (IBAction)shareButtonPressed:(id)sender
