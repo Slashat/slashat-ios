10658f8a4c010b63b2c799f3693bd0d37be6a8ea
Johan Larsson <johan@johanl.se>
2013-09-01 10:26:46 +0200

    Nya filer

---
+++ Slashat/StackScrollViewControllers/SlashatLiveViewController~iPad.h
@@ -0,0 +1,13 @@
+//
+//  SlashatLiveViewController~iPad.h
+//  Slashat
+//
+//  Created by Johan Larsson on 2013-09-01.
+//  Copyright (c) 2013 Johan Larsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlashatLiveViewController_iPad : UIViewController
+
+@end
---
+++ Slashat/StackScrollViewControllers/SlashatLiveViewController~iPad.m
@@ -0,0 +1,38 @@
+//
+//  SlashatLiveViewController~iPad.m
+//  Slashat
+//
+//  Created by Johan Larsson on 2013-09-01.
+//  Copyright (c) 2013 Johan Larsson. All rights reserved.
+//
+
+#import "SlashatLiveViewController~iPad.h"
+
+@interface SlashatLiveViewController_iPad ()
+
+@end
+
+@implementation SlashatLiveViewController_iPad
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
