4caf5c0a51298e1c7993ab41a809acc0c55d2253
Johan Larsson <johan@johanl.se>
2013-09-28 14:58:30 +0200

    Modell för SlashatHighFiveUser

---
+++ Slashat/models/SlashatHighFiveUser.h
@@ -0,0 +1,22 @@
+//
+//  SlashatHighFiveUser.h
+//  Slashat
+//
+//  Created by Johan Larsson on 2013-09-28.
+//  Copyright (c) 2013 Johan Larsson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SlashatHighFiveUser : NSObject
+
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *userName;
+@property (strong, nonatomic) NSString *userId;
+
+@property (strong, nonatomic) NSURL *profilePicture;
+@property (strong, nonatomic) NSURL *qrCode;
+
+@property (strong, nonatomic) NSArray *highFivers;
+
+@end
---
+++ Slashat/models/SlashatHighFiveUser.m
@@ -0,0 +1,13 @@
+//
+//  SlashatHighFiveUser.m
+//  Slashat
+//
+//  Created by Johan Larsson on 2013-09-28.
+//  Copyright (c) 2013 Johan Larsson. All rights reserved.
+//
+
+#import "SlashatHighFiveUser.h"
+
+@implementation SlashatHighFiveUser
+
+@end
