2843091da35531f1e75b14d7be9eff7eed9c1ae2
Johan Larsson <johan@johanl.se>
2013-10-29 09:19:25 +0100

    Lagt till saknade konstanter till SampleAPIKey.h

--- Slashat/SampleAPIKey.h
+++ Slashat/SampleAPIKey.h
@@ -1,5 +1,10 @@
-// Supply your own Bambuser transcode api key in order to enable the video player
+// Döp om denna fil till APIKey.h
+
 #define BAMBUSER_TRANSCODE_API_KEY                  @""
 #define BAMBUSER_TRANSCODE_URL                      @""
 
 #define GOOGLE_CALENDAR_API_KEY                     @""
+
+#define GOOGLE_CALENDAR_API_KEY                     @""
+
+#define SLASHAT_API_URL                             @""
