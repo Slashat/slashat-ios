269068d8008cbfc24f0358df97ba8cfba5e8e9ab
Johan Larsson <johan@johanl.se>
2013-10-31 20:51:16 +0100

    Första unit-testet är tillagt!

--- SlashatTests/SlashatTests.m
+++ SlashatTests/SlashatTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "SlashatTests.h"
+#import "SlashatHighFiveUser.h"
 
 @implementation SlashatTests
 
@@ -24,9 +25,14 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testHighFiveInstantiationWithNoHighFivers
 {
-    STFail(@"Unit tests are not implemented yet in SlashatTests");
+    NSString *jsonString = @"{ \"highfived_by_name\": null, \"highfived_date\": null, \"highfived_where\": null, \"highfivers\": {}, \"picture\": \"http://forum.slashat.se/download/file.php?avatar=78_1382378593.jpeg\", \"qrcode\": \"https://chart.googleapis.com/chart?chs=480x480&cht=qr&choe=UTF-8&chl=5\", \"user_id\": 5, \"username\": \"kottkrig\" }";
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:nil];
+    
+    SlashatHighFiveUser *highFiveUser = [[SlashatHighFiveUser alloc] initWithAttributes:json];
+    STAssertEqualObjects(highFiveUser.userName, @"kottkrig", @"Username should be kottkrig");
 }
 
 @end
