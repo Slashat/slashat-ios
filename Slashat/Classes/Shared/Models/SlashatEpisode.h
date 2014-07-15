a7177fca5ba7421e3b9e830ba56976de986d9635
Johan Larsson <johan@johanl.se>
2014-07-15 22:54:48 +0200

    Fixat crasch i 32-bitars iOS
    episodeNumber låg kvar som assign sedan den var int

--- Slashat/Classes/Shared/Models/SlashatEpisode.h
+++ Slashat/Classes/Shared/Models/SlashatEpisode.h
@@ -10,14 +10,14 @@
 
 @interface SlashatEpisode : NSObject
 
-@property (assign,nonatomic) NSNumber *episodeNumber;
-@property (strong,nonatomic) NSString *title;
-@property (strong,nonatomic) NSString *itemDescription;
-@property (strong,nonatomic) NSString *showNotes;
-@property (strong,nonatomic) NSURL *link;
-@property (strong,nonatomic) NSDate *pubDate;
-@property (strong,nonatomic) NSString *guid;
-@property (strong,nonatomic) NSURL *mediaUrl;
-@property (strong,nonatomic) NSURL *podcastImage;
+@property (strong, nonatomic) NSNumber *episodeNumber;
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *itemDescription;
+@property (strong, nonatomic) NSString *showNotes;
+@property (strong, nonatomic) NSURL *link;
+@property (strong, nonatomic) NSDate *pubDate;
+@property (strong, nonatomic) NSString *guid;
+@property (strong, nonatomic) NSURL *mediaUrl;
+@property (strong, nonatomic) NSURL *podcastImage;
 
 @end
